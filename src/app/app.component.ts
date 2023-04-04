381b8658b46e58f5ec0f9283ba2dd7e15d09a541
Simon Pletersek <pletersek.si@gmail.com>
2023-04-04 11:34:08 +0200

    Succesfully Posting and then getting the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
-
+  loadedPosts: Post[] = [];
+  url =
+    'https://ng-complete-guide-9ebd9-default-rtdb.europe-west1.firebasedatabase.app/';
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
+    this.http
+      .post<{ name: string }>(`${this.url}/posts.json`, postData)
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
     console.log(postData);
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(`${this.url}/posts.json`)
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        this.loadedPosts = posts;
+      });
   }
 }
